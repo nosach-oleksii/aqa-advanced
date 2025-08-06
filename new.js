ab5fb5e79f727105645246140f646628c70fb553
nosach-oleksii <nosach.oleksii@gmail.com>
2025-08-06 20:34:26 +0200

    Added file index.js using chalk.

---
+++ new.js
@@ -0,0 +1,74 @@
+/*let y = 5
+const x = 10
+var z 
+
+z = y + x */
+
+/*let a = "Hello world"
+let b = 'Hello'
+let c = `Hello ${b}`
+let d = "Hello : \"World/" "
+//console.log(c)
+
+const e = 41
+const f = 3.14
+
+console.log(1/0)
+console.log("hello" * 2)
+
+0.1 + 0.2 === 0.3 //false
+console.log(0.1 + 0.2)
+
+Number("42")
+
+parseInt("42",10)
+parceFloat("42")
++"42"
+parceFloat("3.14abc")
+parceFloat("abc") // Nan
+
+toFixid(2)
+Math.round()
+
+const isActive = true ;
+if ( isActive ) {
+    console.log(isActive)
+}*/
+
+/*let x
+let a = null
+console.log(x)
+
+console.log(null == undefined)
+
+const s1 = Symbol("id")
+const s2 = Symbol("id")
+console.log(s1 === s2)
+
+let a = 5
+let b = 10
+let z 
+z = a + b
+console.log(z)
+z = b - a
+console.log(z)
+z = a * b
+console.log(z)
+z = b / a
+console.log(z)
+z = a % b
+console.log(z)
+z = b ** a
+console.log(z)
+
+console.log(7%3)
+console.log(5%10)
+
+a += 5
+console.log(a)*/
+
+let a = 2
+let b = '"2'
+
+console.log ( a !== b )
+
