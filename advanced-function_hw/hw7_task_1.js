ecf0bc84bf5397da3ff6e3e2b72b8db0488989fc
nosach-oleksii <nosach.oleksii@gmail.com>
2025-09-03 13:46:09 +0200

    Add objects-hw with home work tasks 1 - 5

---
+++ advanced-function_hw/hw7_task_1.js
@@ -0,0 +1,38 @@
+// Завдання 1
+// 1. Створіть функцію handleNum яка буде приймати 3 параметри.
+//      a. число
+//      b. Колбек функцію яку треба викликати якщо передане число парне
+//      c. Колбек функцію яку треба викликати якщо передане число непарне
+// 2. Створіть ще дві функції які ви будете передавати у якості колбеків, 
+// наприклад handleEven та handleOdd. Кожна з них має виводити просте 
+// повідомлення в консоль. Наприклад handleEven буде виводити текст 
+// “number is even”, a handleOdd буде виводити текст “number is odd”
+// 3. Викличте функцію handleNum і передайте в якості аргументів довільне
+//  число і дві функції які ви створили раніше
+
+// колбек для парних чисел
+function handleEven() {
+    console.log("number is even");
+}
+
+// колбек для не парних чисел
+function handleOdd() {
+    console.log("number is odd");
+}
+
+// основна функція
+function handleNum(num, callbackEven, callbackOdd){
+    if (num % 2 === 0) {
+        callbackEven();
+    } else {
+        callbackOdd();
+    }
+}
+
+// виклик функцій
+handleNum(5, handleEven, handleOdd)
+handleNum(8, handleEven, handleOdd)
+
+const numbers = [1, 2, 3, 4, 5];
+const doubledNumbers = numbers.map((number) => number * 2);
+console.log(doubledNumbers)
---
+++ function_hw/hw6_task_1.js
@@ -0,0 +1,30 @@
+// Завдання 1
+// Створіть функцію яка приймає два параметри: width і height.
+// Усередині функції обчисліть площу прямокутника, 
+// перемноживши width на height та поверніть результат з функції.
+// Викличте вашу функцію з аргументами (наприклад 5 і 10 і виведіть 
+//     результат (площу прямокутника) в консоль.
+// Реалізуйте функцію трьома способами (function declaration, 
+//     function expression, arrow function)
+
+// function declaration
+function getAreaRectangle1(width, height){
+    let area = width * height;
+    return area;
+}
+
+let result = getAreaRectangle1(3, 5);
+console.log('Area:', result)
+
+
+// function expression
+const getAreaRectangle2 = function(width, height){
+    return "Area: " + width * height;
+}
+
+console.log(getAreaRectangle2(5, 7))
+
+
+// arrow function
+const getAreaRectangle3 = (width, height) => "Area: " + width * height
+console.log(getAreaRectangle3(2, 8))
