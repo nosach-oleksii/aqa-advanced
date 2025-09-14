927ce7205a63d3964bd5451a88d1aab83945c064
nosach-oleksii <nosach.oleksii@gmail.com>
2025-09-14 15:50:56 +0200

    Test commit

---
+++ jsBasics/lesson_4.js
@@ -0,0 +1,20 @@
+// let globalVar = 'Ця змінна доступна скрізь';
+// console.log(globalVar);
+// // const
+// // var
+
+// function great() {
+// 	var message = 'Привіт';
+// 	console.log(message);
+// }
+// great();
+// console.log(message);
+
+// function bar() {
+// 	if (true) {
+// 		var a = 1;
+// 		let b = 2;
+// 	}
+// 	console.log(a);
+// }
+// bar();
---
+++ objects_hw/hw9_task_2.js
@@ -0,0 +1,18 @@
+// Завдання 2
+// 1. Створіть об'єкт book з такими властивостями:
+//      a. title: рядок, представляє назву книги.
+//      b. author: рядок, представляє автора книги.
+//      c. year: рядок, представляє ім'я власника автомобіля
+// 2. Деструктуризуйте назву та автора книги з об'єкта book.
+// 3. Виведіть деструктуризовані змінні в консоль
+
+const book = {
+    title: "Some names of book",
+    author: "Some Author",
+    year: 2025
+};
+
+const { title, author } = book;
+
+console.log("Title:", title);
+console.log("Author:", author);
